3c8077b8de102c0e837c63ee78cf92c005445604
peternatewood <peternatewood@tutanota.com>
2019-09-23 15:30:39 -0400

    Update ActorBase with _deal_damage method

--- scripts/ActorBase.gd
+++ scripts/ActorBase.gd
@@ -7,12 +7,18 @@ func _ready():
 	self.connect("body_entered", self, "_on_body_entered")
 
 
+func _deal_damage(amount: int):
+	hitpoints -= amount
+	if hitpoints <= 0:
+		queue_free()
+
+
 func _on_body_entered(body):
 	if body is EnergyBolt:
-		hitpoints -= 10
+		_deal_damage(10)
 		body.destroy()
 	else:
-		hitpoints -= 1
+		_deal_damage(1)
 
 
 const EnergyBolt = preload("EnergyBolt.gd")
