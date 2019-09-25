df7cbc30e6cbc2222c3a058e279e133e7b5991e7
peternatewood <peternatewood@tutanota.com>
2019-09-25 16:00:13 -0400

    Add _fire_missile_weapon method to ShipBase

--- scripts/ShipBase.gd
+++ scripts/ShipBase.gd
@@ -1,6 +1,7 @@
 extends "res://scripts/ActorBase.gd"
 
 onready var energy_weapon_hardpoints = get_node("Energy Weapon Hardpoints 1").get_children()
+onready var missile_weapon_hardpoints = get_node("Missile Weapon Hardpoints").get_children()
 onready var shield_front = get_node("Shield Front")
 onready var shield_left = get_node("Shield Left")
 onready var shield_rear = get_node("Shield Rear")
@@ -8,6 +9,8 @@ onready var shield_right = get_node("Shield Right")
 
 var energy_weapon_countdown: float = 0.0
 var energy_weapon_index: int = 0
+var missile_weapon_countdown: float = 0.0
+var missile_weapon_index: int = 0
 var throttle: float
 var torque_vector: Vector3
 
@@ -39,6 +42,20 @@ func _fire_energy_weapon():
 	energy_weapon_index = (energy_weapon_index + 1) % energy_weapon_hardpoints.size()
 
 
+func _fire_missile_weapon():
+	var missile = MISSILE.instance()
+	missile.set_collision_layer(collision_layer)
+	missile.set_collision_mask(collision_mask)
+
+	get_tree().get_root().add_child(missile)
+	missile.transform.origin = missile_weapon_hardpoints[missile_weapon_index].global_transform.origin
+	missile.look_at(missile.transform.origin - transform.basis.z, transform.basis.y)
+	missile.add_speed(get_linear_velocity().length())
+
+	missile_weapon_countdown = MISSILE_WEAPON_DELAY
+	missile_weapon_index = (missile_weapon_index + 1) % missile_weapon_hardpoints.size()
+
+
 func _physics_process(delta):
 	add_torque(TURN_SPEED * torque_vector)
 	apply_central_impulse(throttle * -transform.basis.z)
@@ -48,9 +65,14 @@ func _process(delta):
 	if energy_weapon_countdown != 0:
 		energy_weapon_countdown = max(0, energy_weapon_countdown - delta)
 
+	if missile_weapon_countdown != 0:
+		missile_weapon_countdown = max(0, missile_weapon_countdown - delta)
+
 
 const ACCELERATION: float = 0.1
 const ENERGY_BOLT = preload("res://models/Energy_Bolt.tscn")
 const ENERGY_WEAPON_DELAY: float = 0.1
+const MISSILE = preload("res://models/missile/missile.dae")
+const MISSILE_WEAPON_DELAY: float = 1.2
 const MAX_THROTTLE: float = 1.0
 const TURN_SPEED: float = 2.5
