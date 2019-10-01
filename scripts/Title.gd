cba08f38eb5820669c76271571654e01764faa10
peternatewood <peternatewood@tutanota.com>
2019-10-01 12:41:03 -0400

    Load mission with scene loader from Title scene

--- scripts/Title.gd
+++ scripts/Title.gd
@@ -1,16 +1,17 @@
 extends Control
 
-onready var new_game_button = get_node("Container/New Game")
 onready var exit_button = get_node("Container/Exit")
+onready var loader = get_node("/root/SceneLoader")
+onready var new_game_button = get_node("Container/New Game")
 
 
 func _ready():
-	new_game_button.connect("pressed", self, "_on_new_game_pressed")
 	exit_button.connect("pressed", self, "_on_exit_pressed")
+	new_game_button.connect("pressed", self, "_on_new_game_pressed")
 
 
 func _on_new_game_pressed():
-	get_tree().change_scene("res://mission.tscn")
+	loader.change_scene("res://mission.tscn")
 
 
 func _on_exit_pressed():
