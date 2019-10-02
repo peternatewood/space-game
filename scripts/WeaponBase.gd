5bc00bc38e7e723bb31c5253b1fc3b0c2774aa5e
peternatewood <peternatewood@tutanota.com>
2019-10-02 11:23:27 -0400

    Add base class for weapons

---
+++ scripts/WeaponBase.gd
@@ -0,0 +1,20 @@
+extends RigidBody
+
+var damage_hull: int = 15
+var damage_shield: int = 10
+var fire_delay: float = 0.3 # In seconds
+var life: float = 5.0 # In seconds
+var speed: float = 80.0
+
+
+func _process(delta):
+	life -= delta
+	if life <= 0:
+		queue_free()
+
+
+# PUBLIC
+
+
+func destroy():
+	queue_free()
