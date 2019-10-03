979f931bc89d92ee5d9f8d8683928671e7d8032d
peternatewood <peternatewood@tutanota.com>
2019-10-03 17:21:48 -0400

    Add MissionController method to get ships' alignment to each other

--- scripts/MissionController.gd
+++ scripts/MissionController.gd
@@ -2,6 +2,12 @@ extends Node
 
 onready var loader = get_node("/root/SceneLoader")
 
+var factions = {
+	"frog": { "hawk": FRIENDLY, "spider": NEUTRAL },
+	"hawk": { "frog": FRIENDLY, "spider": HOSTILE },
+	"spider": { "hawk": HOSTILE, "frog": NEUTRAL }
+}
+
 
 func _ready():
 	loader.connect("scene_loaded", self, "_on_scene_loaded")
@@ -10,3 +16,16 @@ func _ready():
 
 func _on_scene_loaded():
 	set_process(true)
+
+
+# PUBLIC
+
+
+func get_alignment(factionA: String, factionB: String):
+	if factions.has_key(factionA):
+		return factions[factionA].get(factionB, -1)
+
+	return -1
+
+
+enum { NEUTRAL, FRIENDLY, HOSTILE }
