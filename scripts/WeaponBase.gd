65e8a09087d4efbab1a26ae0c52908225f1ed795
peternatewood <peternatewood@tutanota.com>
2019-10-03 10:34:37 -0400

    Add notes to WeaponBase about calculating velocity

--- scripts/WeaponBase.gd
+++ scripts/WeaponBase.gd
@@ -19,3 +19,16 @@ func _process(delta):
 
 func destroy():
 	queue_free()
+
+
+"""
+NOTE: weight doesn't matter if gravity is 0
+mass: 1.0 | speed: 80 | velocity: 1.33333 | speed/vel: 60
+mass: 0.5 | speed: 80 | velocity: 2.66667 | speed/vel: 30
+mass: 0.2 | speed: 80 | velocity: 6.66667 | speed/vel: 12
+mass: 0.1 | speed: 80 | velocity: 13.3333 | speed/vel: 6
+
+mass: 0.2 | speed: 20 | velocity: 1.66667 | speed/vel: 12
+
+To get the end velocity, divide the speed property by (mass * 60)
+"""
