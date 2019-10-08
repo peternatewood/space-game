45c2cfcd1f67eedbb6cf3902abb467352a30fadb
peternatewood <peternatewood@tutanota.com>
2019-10-08 15:30:03 -0400

    Improve scene loader to use threading

--- scripts/SceneLoader.gd
+++ scripts/SceneLoader.gd
@@ -1,8 +1,7 @@
 extends Node
 
 var current_scene
-var loader
-var wait_frames: int
+var thread: Thread = Thread.new()
 
 
 func _ready():
@@ -10,79 +9,62 @@ func _ready():
 	current_scene = root.get_child(root.get_child_count() - 1)
 
 
-func _process(delta):
-	if loader == null:
-		set_process(false)
-		return
-
-	if wait_frames > 0:
-		wait_frames -= 1
-		return
-
-	var time = OS.get_ticks_msec()
-	while OS.get_ticks_msec() < time + TIME_MAX:
+func _load_scene(loader):
+	while true:
 		var error = loader.poll()
 
 		if error == ERR_FILE_EOF:
-			# Unload the loading bar scene
-			var root = get_tree().get_root()
-			current_scene = root.get_child(root.get_child_count() - 1)
-			current_scene.queue_free()
-
-			# Set the new scene
+			# Get the new scene
 			var resource = loader.get_resource()
-			loader = null
-			_set_new_scene(resource)
+			call_deferred("_set_new_scene")
 
-			emit_signal("scene_loaded")
-			break
+			return resource
 		elif error == OK:
-			_update_progress()
+			_update_progress(loader)
 		else:
 			# TODO: handle error somehow
-			loader = null
-			break
+			print("Loader error! " + error)
+			return
 
 
-func _set_new_scene(scene_resource):
-	current_scene = scene_resource.instance()
-	get_tree().get_root().add_child(current_scene)
+func _set_new_scene():
+	var scene_resource = thread.wait_to_finish()
+
+	if scene_resource == null:
+		print("Scene failed to load!")
+	else:
+		# Unload the loading bar scene
+		var root = get_tree().get_root()
+		current_scene = root.get_child(root.get_child_count() - 1)
+		current_scene.queue_free()
+
+		# Set the new scene
+		current_scene = scene_resource.instance()
+		get_tree().get_root().add_child(current_scene)
+		emit_signal("scene_loaded")
 
 
-func _update_progress():
+func _update_progress(loader):
 	emit_signal("loading_progressed", loader.get_stage())
 
 
 # PUBLIC
 
 
-func get_stage_count():
-	if loader == null:
-		return 1
-
-	return loader.get_stage_count()
-
-
-func change_scene(path):
-	loader = ResourceLoader.load_interactive(path)
-	if loader == null:
-		# TODO: handle error somehow
-		print("Loader is null!")
-		return
-	set_process(true)
+func change_scene(path: String):
 	current_scene.queue_free()
 
 	var tree = get_tree()
-	#tree.change_scene("res://loading.tscn")
 	tree.change_scene_to(LOADING_SCENE)
 	if tree.paused:
 		tree.set_pause(false)
 
-	wait_frames = 1
+	var loader = ResourceLoader.load_interactive(path)
+	emit_signal("stage_count", loader.get_stage_count())
+	thread.start(self, "_load_scene", loader)
 
 
 signal loading_progressed
 signal scene_loaded
 
 const LOADING_SCENE = preload("res://loading.tscn")
-const TIME_MAX: int = 10
