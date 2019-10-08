7602bf0c5b45f197566e8a7668d4bbc715474879
peternatewood <peternatewood@tutanota.com>
2019-10-08 16:40:53 -0400

    Add hidden shield outline icons to hud

--- scripts/ShipIcon.gd
+++ scripts/ShipIcon.gd
@@ -7,6 +7,20 @@ onready var shield_icons: Array = [
 	get_node("Shield Left"),
 	get_node("Shield Right")
 ]
+onready var shield_outline_icons: Array = [
+	get_node("Shield Front Outline"),
+	get_node("Shield Rear Outline"),
+	get_node("Shield Left Outline"),
+	get_node("Shield Right Outline")
+]
+
+
+func _ready():
+	for icon in shield_outline_icons:
+		icon.hide()
+
+
+# PUBLIC
 
 
 func set_overhead_icon(icon):
