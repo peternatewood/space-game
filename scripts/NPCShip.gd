de276612bf15c4d2ab23b9ae68ee75b784168245
peternatewood <peternatewood@tutanota.com>
2019-10-14 18:16:31 -0400

    Turn npc ships toward current target

--- scripts/NPCShip.gd
+++ scripts/NPCShip.gd
@@ -11,14 +11,25 @@ func _on_scene_loaded():
 
 func _process(delta):
 	if has_target:
-		var raycast_collider = target_raycast.get_collider()
-		if raycast_collider == current_target:
-			_fire_energy_weapon()
+		_turn_towards_target(delta)
+
+		if behavior_state != PASSIVE:
+			var raycast_collider = target_raycast.get_collider()
+			if raycast_collider == current_target:
+				_fire_energy_weapon()
 	else:
 		# TODO: find a target, patrol, or do something else
 		pass
 
 	._process(delta)
+
+
+func _turn_towards_target(delta):
+	var to_target = (current_target.transform.origin - transform.origin).normalized()
+	var x_dot = transform.basis.x.dot(to_target)
+	var y_dot = transform.basis.y.dot(to_target)
+
+	torque_vector = transform.basis.x * y_dot - transform.basis.y * x_dot
 
 
 enum { PASSIVE, PATROL, DEFEND, ATTACK }
