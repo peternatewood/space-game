4686b0bd287f4b88ec90c3a5073c6706f1a615af
peternatewood <peternatewood@tutanota.com>
2019-10-14 18:15:36 -0400

    Add behavior state property to NPCShip

--- scripts/NPCShip.gd
+++ scripts/NPCShip.gd
@@ -1,5 +1,7 @@
 extends "res://scripts/ShipBase.gd"
 
+var behavior_state: int = PASSIVE
+
 
 func _on_scene_loaded():
 	_set_current_target(get_tree().get_root().get_node("Mission Controller/Player"))
@@ -12,8 +14,13 @@ func _process(delta):
 		var raycast_collider = target_raycast.get_collider()
 		if raycast_collider == current_target:
 			_fire_energy_weapon()
+	else:
+		# TODO: find a target, patrol, or do something else
+		pass
 
 	._process(delta)
 
+
+enum { PASSIVE, PATROL, DEFEND, ATTACK }
 
 const LINE_OF_FIRE_SQ: float = 4.0 # Squared to make processing faster
