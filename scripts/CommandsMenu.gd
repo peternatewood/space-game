52594886d300a42b582d42b3813f09bde12461e7
peternatewood <peternatewood@tutanota.com>
2019-10-16 11:26:43 -0400

    Add number press handler to commands menu

--- scripts/CommandsMenu.gd
+++ scripts/CommandsMenu.gd
@@ -11,6 +11,11 @@ var command_type: int = NONE
 var timeout_countdown: float = 0.0
 
 
+func _handle_number_press(number: int):
+	print(str(number))
+	timeout_countdown = TIMEOUT_DELAY
+
+
 func _input(event):
 	if event.is_action("toggle_communcations_menu") and event.pressed:
 		if visible:
@@ -18,6 +23,23 @@ func _input(event):
 		else:
 			show()
 			timeout_countdown = TIMEOUT_DELAY
+	elif event is InputEventKey and event.pressed:
+		# Handle number inputs
+		match event.scancode:
+			KEY_1, KEY_KP_1:
+				_handle_number_press(1)
+			KEY_2, KEY_KP_2:
+				_handle_number_press(2)
+			KEY_3, KEY_KP_3:
+				_handle_number_press(3)
+			KEY_4, KEY_KP_4:
+				_handle_number_press(4)
+			KEY_5, KEY_KP_5:
+				_handle_number_press(5)
+			KEY_6, KEY_KP_6:
+				_handle_number_press(6)
+			KEY_7, KEY_KP_7:
+				_handle_number_press(7)
 
 
 func _process(delta):
