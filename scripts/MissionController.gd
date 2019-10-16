62cb0f329697768472493293333be4b279637719
peternatewood <peternatewood@tutanota.com>
2019-10-16 12:11:13 -0400

    Add MissionController script to get ships the player can command

--- scripts/MissionController.gd
+++ scripts/MissionController.gd
@@ -2,6 +2,8 @@ extends Node
 
 enum { NEUTRAL, FRIENDLY, HOSTILE }
 
+export (NodePath) var player_path
+
 onready var loader = get_node("/root/SceneLoader")
 
 var factions = {
@@ -9,6 +11,7 @@ var factions = {
 	"hawk": { "frog": FRIENDLY, "spider": HOSTILE },
 	"spider": { "hawk": HOSTILE, "frog": NEUTRAL }
 }
+var player
 var targets_container
 var waypoints: Array = []
 var waypoints_container
@@ -24,6 +27,7 @@ func _on_scene_loaded():
 	targets_container = get_node("Targets Container")
 	waypoints_container = get_node("Waypoints Container")
 	waypoints = waypoints_container.get_children()
+	player = get_node(player_path)
 
 	set_process(true)
 
@@ -39,6 +43,17 @@ func get_alignment(factionA: String, factionB: String):
 		return factions[factionA].get(factionB, -1)
 
 	return -1
+
+
+func get_commandable_ships():
+	var commandable_ships: Array = []
+
+	for target in get_targets():
+		# TODO: also check some other property like rank or ship class to determine whether player can command or not
+		if target != player and get_alignment(player.faction, target.faction) == FRIENDLY:
+			commandable_ships.append(target)
+
+	return commandable_ships
 
 
 func get_next_waypoint_pos(index: int):
