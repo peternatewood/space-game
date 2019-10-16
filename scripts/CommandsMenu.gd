9105ed30831727f3b4d9ac8734c8509193e7c12f
peternatewood <peternatewood@tutanota.com>
2019-10-16 11:22:29 -0400

    Hide commands menu after four seconds of inactivity

--- scripts/CommandsMenu.gd
+++ scripts/CommandsMenu.gd
@@ -8,6 +8,7 @@ onready var ships_menu = get_node("Ships List")
 onready var wings_menu = get_node("Wings List")
 
 var command_type: int = NONE
+var timeout_countdown: float = 0.0
 
 
 func _input(event):
@@ -16,3 +17,19 @@ func _input(event):
 			hide()
 		else:
 			show()
+			timeout_countdown = TIMEOUT_DELAY
+
+
+func _process(delta):
+	if timeout_countdown > 0:
+		timeout_countdown -= delta
+
+		if timeout_countdown <= 0:
+			hide()
+			root_menu.show()
+			ship_commands.hide()
+			ships_menu.hide()
+			wings_menu.hide()
+
+
+var TIMEOUT_DELAY: float = 4.0 # In seconds
