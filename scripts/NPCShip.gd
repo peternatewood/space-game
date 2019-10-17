05c06f173a763ce6df153e1441d199c4a0e6697b
peternatewood <peternatewood@tutanota.com>
2019-10-17 11:56:37 -0400

    Refactor NPCShip.set_command in prep for handling multiple orders

--- scripts/NPCShip.gd
+++ scripts/NPCShip.gd
@@ -68,7 +68,7 @@ func _process(delta):
 				if has_target:
 					_attack_current_target()
 				else:
-					o.type = ORDER_TYPE.PASSIVE
+					_set_current_target(o.target)
 			ORDER_TYPE.DEFEND, ORDER_TYPE.COVER_ME:
 				# NOTE: when switching to a DEFEND order, the ship should untarget whatever it's currently targeting
 				if has_target:
@@ -144,45 +144,46 @@ func _turn_towards_target(target_pos: Vector3):
 # PUBLIC
 
 
+# Return true if command successful, false if unable to comply for whatever reason
 func set_command(command: int, commander):
 	var alignment: int = mission_controller.get_alignment(faction, commander.faction)
 	var target_alignment: int = -1
+	var new_order = Order.new(command)
+
 	if commander.has_target:
 		target_alignment = mission_controller.get_alignment(faction, commander.current_target.faction)
+		new_order.target = commander.current_target
+	else:
+		new_order.target = commander
 
 	match command:
 		ORDER_TYPE.ATTACK:
 			if not commander.has_target:
 				print("No target selected!")
+				return false
 			elif target_alignment == mission_controller.FRIENDLY:
 				print("Cannot attack a friendly target!")
-			else:
-				orders[0].type = ORDER_TYPE.ATTACK
-				_set_current_target(commander.current_target)
+				return false
 		ORDER_TYPE.DEFEND:
 			if not commander.has_target:
 				print("No target selected!")
+				return false
 			elif target_alignment == mission_controller.HOSTILE:
 				print("Cannot defend a hostile target!")
+				return false
 			else:
-				orders[0].type = ORDER_TYPE.DEFEND
-				orders[0].target = commander.current_target
 				commander.current_target.connect("destroyed", self, "_defended_target_destroyed", [ 0 ])
 		ORDER_TYPE.IGNORE:
 			if not commander.has_target:
 				print("No target selected!")
-			else:
-				orders[0].type = ORDER_TYPE.IGNORE
-				orders[0].target = commander.current_target
+				return false
 		ORDER_TYPE.COVER_ME:
 			if alignment == mission_controller.HOSTILE:
 				print("Cannot cover a hostile target!")
-			else:
-				orders[0].type = ORDER_TYPE.COVER_ME
-				orders[0].target = commander
-		ORDER_TYPE.ATTACK_ANY:
-			orders[0].type = ORDER_TYPE.ATTACK_ANY
-			print(name + " engaging at will")
+				return false
+
+	orders[0] = new_order
+	return true
 
 
 const LINE_OF_FIRE_SQ: float = 4.0 # Squared to make processing faster
