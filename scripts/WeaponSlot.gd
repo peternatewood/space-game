f83223970b4f45088726d0f2c1449063130606cc
peternatewood <peternatewood@tutanota.com>
2019-10-21 18:21:31 -0400

    Refactor weapon slot script

--- scripts/WeaponSlot.gd
+++ scripts/WeaponSlot.gd
@@ -5,7 +5,7 @@ enum TYPE { SHIP, ENERGY_WEAPON, MISSILE_WEAPON }
 export (TYPE) var slot_type
 
 onready var area = get_node("Area")
-onready var highlight = get_node("Area/Highlight")
+onready var highlight_polygon = get_node("Area/Highlight")
 onready var sprite = get_node("Area/Sprite")
 
 
@@ -14,11 +14,19 @@ func _ready():
 	area.connect("area_exited", self, "_on_area_exited")
 
 
-func _on_area_entered(area):
-	if area.has_meta("type") and area.get_meta("type") == slot_type:
-		highlight.show()
+# PUBLIC
 
 
-func _on_area_exited(area):
-	if area.has_meta("type") and area.get_meta("type") == slot_type:
-		highlight.hide()
+func highlight(toggle_on: bool):
+	if toggle_on:
+		highlight_polygon.show()
+	else:
+		highlight_polygon.hide()
+
+
+func is_area_same_type(area):
+	return area.has_meta("type") and area.get_meta("type") == slot_type
+
+func set_icon(image_resource):
+	sprite.set_texture(image_resource)
+	sprite.show()
