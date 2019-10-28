936f9b4b9dba8e06eab3596b363a1613f7201f33
peternatewood <peternatewood@tutanota.com>
2019-10-28 16:34:53 -0400

    Add default video settings to GlobalSettings

--- scripts/GlobalSettings.gd
+++ scripts/GlobalSettings.gd
@@ -1,5 +1,19 @@
 extends Object
 
+# Default settings here
+var aniso_filtering = Setting.new("aniso_filtering", 0)
+var borderless = Setting.new("borderless", false)
+var fov = Setting.new("fov", 70)
+var fullscreen = Setting.new("fullscreen", false)
+var hdr = Setting.new("hdr", false)
+var msaa = Setting.new("msaa", 0)
+var reflections = Setting.new("reflections", 2048)
+var resolution = Setting.new("resolution", Vector2(1024, 768))
+var shadows_dir = Setting.new("shadows_dir", 4096)
+var shadows_point = Setting.new("shadows_point", 4096)
+var subsurf_scatter = Setting.new("subsurf_scatter", 0)
+var vsync = Setting.new("vsync", true)
+
 
 # Used to maintain properties of a given setting
 class Setting:
