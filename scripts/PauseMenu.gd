0fbc9d25d9a48b66d3e1aa977f15fa14e13cca0c
peternatewood <peternatewood@tutanota.com>
2019-10-29 16:29:31 -0400

    Add pause menu with options menu

---
+++ scripts/PauseMenu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+onready var options_menu = get_node("Options Menu")
+
+
+func _ready():
+	var resume_button = get_node("PanelContainer/Buttons Container/Resume Button")
+	resume_button.connect("pressed", self, "_on_resume_button_pressed")
+
+	var options_button = get_node("PanelContainer/Buttons Container/Options Button")
+	options_button.connect("pressed", self, "_on_options_button_pressed")
+
+	var quit_menu_button = get_node("PanelContainer/Buttons Container/Quit to Main Menu Button")
+	quit_menu_button.connect("pressed", self, "_on_quit_menu_button_pressed")
+
+	var quit_desktop_button = get_node("PanelContainer/Buttons Container/Quit to Desktop Button")
+	quit_desktop_button.connect("pressed", self, "_on_quit_desktop_button_pressed")
+
+
+func _on_resume_button_pressed():
+	hide()
+	get_tree().set_pause(false)
+
+
+func _on_options_button_pressed():
+	pass
+
+
+func _on_quit_menu_button_pressed():
+	pass
+
+
+func _on_quit_desktop_button_pressed():
+	pass
