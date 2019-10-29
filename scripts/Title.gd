65bd79737f8b2c77eb73b7633a90e98fe80342dc
peternatewood <peternatewood@tutanota.com>
2019-10-29 16:04:30 -0400

    Update theme in Title scene when toggling dyslexia

--- scripts/Title.gd
+++ scripts/Title.gd
@@ -3,6 +3,7 @@ extends Control
 onready var loader = get_node("/root/SceneLoader")
 onready var mission_data = get_node("/root/MissionData")
 onready var options_menu = get_node("Options Menu")
+onready var settings = get_node("/root/GlobalSettings")
 
 
 func _ready():
@@ -17,10 +18,18 @@ func _ready():
 	new_game_button.grab_focus()
 
 	options_menu.connect("back_button_pressed", self, "_on_back_button_pressed")
+	settings.connect("dyslexia_toggled", self, "_on_dyslexia_toggled")
 
 
 func _on_back_button_pressed():
 	options_menu.hide()
+
+
+func _on_dyslexia_toggled(toggled_on: bool):
+	if toggled_on:
+		set_theme(settings.OPEN_DYSLEXIC_THEME)
+	else:
+		set_theme(settings.INCONSOLATA_THEME)
 
 
 func _on_exit_pressed():
