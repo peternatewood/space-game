af9c86c7ed129c4b3463cd3b777467d6a256318e
peternatewood <peternatewood@tutanota.com>
2019-10-30 15:08:47 -0400

    Highlight radial menu icon on hover

--- scripts/RadialMenu.gd
+++ scripts/RadialMenu.gd
@@ -1,21 +1,25 @@
 extends Control
 
+onready var current_icon = get_node("Current Icon")
+onready var highlight_control = get_node("Highlight")
+onready var radial_container = get_node("Radial Container")
 
-func set_icons(images: Array):
-	var icons_container = get_node("Icons Container")
-	var radian_increment: float = TAU / images.size()
-	var radians: float = 0
 
-	for image in images:
-		var icon = RADIAL_ICON.instance()
-		icons_container.add_child(icon)
-		icon.set_normal_texture(image)
+func _ready():
+	connect("mouse_entered", self, "_on_mouse_entered")
+	connect("mouse_exited", self, "_on_mouse_exited")
+	current_icon.set_modulate(RadialIcon.NORMAL_COLOR)
 
-		var icon_position = Vector2(HALF_RADIUS * cos(radians), HALF_RADIUS * sin(radians))
-		icon.set_position(icon_position - icon.rect_size / 2)
 
-		radians += radian_increment
+func _on_mouse_entered():
+	current_icon.set_modulate(Color.white)
 
+
+func _on_mouse_exited():
+	current_icon.set_modulate(RadialIcon.NORMAL_COLOR)
+
+
+const RadialIcon = preload("RadialIcon.gd")
 
 const HALF_RADIUS: float = 106.0
 const RADIAL_ICON = preload("res://prefabs/radial_icon.tscn")
