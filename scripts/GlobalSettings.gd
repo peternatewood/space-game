2a1b40d1942cb843f87b1e572e952b15d2a2d94c
peternatewood <peternatewood@tutanota.com>
2019-10-31 13:14:48 -0400

    Add audio percent and mute settings

--- scripts/GlobalSettings.gd
+++ scripts/GlobalSettings.gd
@@ -3,6 +3,12 @@ extends Node
 # Default settings
 var settings: Dictionary = {
 	"aniso_filtering": Setting.new("aniso_filtering", 0),
+	"audio_master_percent": Setting.new("audio_master_percent", 100),
+	"audio_master_mute": Setting.new("audio_master_mute", false),
+	"audio_music_percent": Setting.new("audio_music_percent", 100),
+	"audio_music_mute": Setting.new("audio_music_mute", false),
+	"audio_sound_effects_percent": Setting.new("audio_sound_effects_percent", 100),
+	"audio_sound_effects_mute": Setting.new("audio_sound_effects_mute", false),
 	"borderless": Setting.new("borderless", false),
 	"dyslexia": Setting.new("dyslexia", false),
 	"fov": Setting.new("fov", 70),
@@ -83,6 +89,30 @@ func _update_resolution():
 # PUBLIC
 
 
+func get_audio_master_percent():
+	return settings.audio_master_percent.get_value()
+
+
+func get_audio_master_mute():
+	return settings.audio_master_mute.get_value()
+
+
+func get_audio_music_percent():
+	return settings.audio_music_percent.get_value()
+
+
+func get_audio_music_mute():
+	return settings.audio_music_mute.get_value()
+
+
+func get_audio_sound_effects_percent():
+	return settings.audio_sound_effects_percent.get_value()
+
+
+func get_audio_sound_effects_mute():
+	return settings.audio_sound_effects_mute.get_value()
+
+
 func get_borderless_window():
 	return settings.borderless_window.get_value()
 
@@ -121,6 +151,48 @@ func get_units():
 
 func get_vsync():
 	return settings.vsync.get_value()
+
+
+func set_audio_master_percent(percent: float):
+	settings.audio_master_percent.set_value(percent)
+	AudioServer.set_bus_volume_db(0, MathHelper.percent_to_db(percent))
+
+	_save_settings_to_file()
+
+
+func set_audio_master_mute(toggle_on: bool):
+	settings.audio_master_mute.set_value(toggle_on)
+	AudioServer.set_bus_mute(0, toggle_on)
+
+	_save_settings_to_file()
+
+
+func set_audio_music_percent(percent: float):
+	settings.audio_music_percent.set_value(percent)
+	AudioServer.set_bus_volume_db(1, MathHelper.percent_to_db(percent))
+
+	_save_settings_to_file()
+
+
+func set_audio_music_mute(toggle_on: bool):
+	settings.audio_music_mute.set_value(toggle_on)
+	AudioServer.set_bus_mute(1, toggle_on)
+
+	_save_settings_to_file()
+
+
+func set_audio_sound_effects_percent(percent: float):
+	settings.audio_sound_effects_percent.set_value(percent)
+	AudioServer.set_bus_volume_db(2, MathHelper.percent_to_db(percent))
+
+	_save_settings_to_file()
+
+
+func set_audio_sound_effects_mute(toggle_on: bool):
+	settings.audio_sound_effects_mute.set_value(toggle_on)
+	AudioServer.set_bus_mute(2, toggle_on)
+
+	_save_settings_to_file()
 
 
 func set_borderless_window(toggle_on: bool):
