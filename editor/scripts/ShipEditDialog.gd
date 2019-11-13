09bb305d89ffc8b1f80550007511d3e97469fe1b
peternatewood <peternatewood@tutanota.com>
2019-11-13 14:59:30 -0500

    Add player ship checkbox to ship edit dialog

--- editor/scripts/ShipEditDialog.gd
+++ editor/scripts/ShipEditDialog.gd
@@ -1,30 +1,31 @@
 extends WindowDialog
 
 onready var energy_weapon_labels: Array = [
-	get_node("Ship Edit Grid/Energy Weapon Label 1"),
-	get_node("Ship Edit Grid/Energy Weapon Label 2"),
-	get_node("Ship Edit Grid/Energy Weapon Label 3")
+	get_node("Ship Edit Rows/Ship Edit Grid/Energy Weapon Label 1"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Energy Weapon Label 2"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Energy Weapon Label 3")
 ]
 onready var energy_weapon_options: Array = [
-	get_node("Ship Edit Grid/Energy Weapon Options 1"),
-	get_node("Ship Edit Grid/Energy Weapon Options 2"),
-	get_node("Ship Edit Grid/Energy Weapon Options 3")
+	get_node("Ship Edit Rows/Ship Edit Grid/Energy Weapon Options 1"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Energy Weapon Options 2"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Energy Weapon Options 3")
 ]
-onready var grid = get_node("Ship Edit Grid")
-onready var hitpoints_spinbox = get_node("Ship Edit Grid/Hitpoints SpinBox")
+onready var grid = get_node("Ship Edit Rows/Ship Edit Grid")
+onready var hitpoints_spinbox = get_node("Ship Edit Rows/Ship Edit Grid/Hitpoints SpinBox")
 onready var missile_weapon_labels: Array = [
-	get_node("Ship Edit Grid/Missile Weapon Label 1"),
-	get_node("Ship Edit Grid/Missile Weapon Label 2"),
-	get_node("Ship Edit Grid/Missile Weapon Label 3")
+	get_node("Ship Edit Rows/Ship Edit Grid/Missile Weapon Label 1"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Missile Weapon Label 2"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Missile Weapon Label 3")
 ]
 onready var missile_weapon_options: Array = [
-	get_node("Ship Edit Grid/Missile Weapon Options 1"),
-	get_node("Ship Edit Grid/Missile Weapon Options 2"),
-	get_node("Ship Edit Grid/Missile Weapon Options 3")
+	get_node("Ship Edit Rows/Ship Edit Grid/Missile Weapon Options 1"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Missile Weapon Options 2"),
+	get_node("Ship Edit Rows/Ship Edit Grid/Missile Weapon Options 3")
 ]
-onready var name_lineedit = get_node("Ship Edit Grid/Name LineEdit")
-onready var ship_class_options = get_node("Ship Edit Grid/Ship Class Options")
-onready var wing_lineedit = get_node("Ship Edit Grid/Ship Wing LineEdit")
+onready var name_lineedit = get_node("Ship Edit Rows/Ship Edit Grid/Name LineEdit")
+onready var player_ship_checkbox = get_node("Ship Edit Rows/Player Ship CheckBox")
+onready var ship_class_options = get_node("Ship Edit Rows/Ship Edit Grid/Ship Class Options")
+onready var wing_lineedit = get_node("Ship Edit Rows/Ship Edit Grid/Ship Wing LineEdit")
 
 
 func fill_ship_info(ship):
@@ -66,22 +67,17 @@ func has_point(point: Vector2):
 
 
 func prepare_options(mission_data):
-	var ship_model_index: int = 0
 	for name in mission_data.ship_models.keys():
-		ship_class_options.add_item(name, ship_model_index)
-		ship_model_index += 1
+		ship_class_options.add_item(name)
 
-	var energy_model_index: int = 0
 	for name in mission_data.energy_weapon_models.keys():
 		for option in energy_weapon_options:
-			option.add_item(name, energy_model_index)
-		energy_model_index += 1
+			option.add_item(name)
 
-	var missile_model_index: int = 0
 	for name in mission_data.missile_weapon_models.keys():
 		for option in missile_weapon_options:
-			option.add_item(name, missile_model_index)
-		missile_model_index += 1
+			option.add_item(name)
 
 
 const AttackShipBase = preload("res://scripts/AttackShipBase.gd")
+const Player = preload("res://scripts/Player.gd")
