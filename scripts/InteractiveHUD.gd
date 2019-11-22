f19ce824be1ca80e044d62981716f316dde4d7d1
peternatewood <peternatewood@tutanota.com>
2019-11-22 16:58:13 -0500

    Update options menu hud to show hidden elements

--- scripts/InteractiveHUD.gd
+++ scripts/InteractiveHUD.gd
@@ -1,10 +1,26 @@
 extends Control
 
+onready var edge_target_icon = get_node("Edge Target Icon")
 onready var settings = get_node("/root/GlobalSettings")
+onready var target_class = get_node("Target View Container/Target View Rows/Target Class")
+onready var target_icon = get_node("Target Icon")
+onready var target_name = get_node("Target View Container/Target View Rows/Target Name")
 
 
 func _ready():
 	set_palette(settings.get_hud_palette())
+
+	# Show normally hidden nodes
+	get_node("Communications Menu").show()
+	get_node("Target Overhead").show()
+	get_node("Target Details Minimal").show()
+	get_node("Target View Container").show()
+
+	edge_target_icon.show()
+	target_icon.show()
+
+	settings.connect("ui_colors_changed", self, "update_colored_icons")
+	update_colored_icons()
 
 
 func set_palette(palette: Dictionary):
@@ -25,6 +41,15 @@ func set_palette(palette: Dictionary):
 		else:
 			var color: Color = palette.get(path, palette.default)
 			node.set_self_modulate(color)
+
+
+func update_colored_icons():
+	var target_color = settings.get_interface_color(2)
+
+	edge_target_icon.set_modulate(target_color)
+	target_class.set_modulate(target_color)
+	target_icon.set_modulate(target_color)
+	target_name.set_modulate(target_color)
 
 
 const COLORABLE_NODE_PATHS: Array = [
