80a20f0ee72d5bd4ea94813d5c5abd799fe990ef
peternatewood <peternatewood@tutanota.com>
2019-11-26 11:56:16 -0500

    Fix WaypointGroupsDialog to populate lineedits properly

--- editor/scripts/WaypointGroupsDialog.gd
+++ editor/scripts/WaypointGroupsDialog.gd
@@ -28,9 +28,23 @@ func get_group_names():
 
 
 func populate_row_options(group_names: Array):
-	for group_name in group_names:
-		var group_line_edit = LineEdit.new()
-		group_rows.add_child(group_line_edit)
+	var current_group_count = group_rows.get_child_count()
+	var new_group_count = group_names.size()
 
-		group_line_edit.set_h_size_flags(SIZE_EXPAND_FILL)
-		group_line_edit.set_text(group_name)
+	for index in range(max(new_group_count, current_group_count)):
+		if index >= current_group_count:
+			# Add lineedits
+			var group_line_edit = LineEdit.new()
+			group_rows.add_child(group_line_edit)
+
+			group_line_edit.set_h_size_flags(SIZE_EXPAND_FILL)
+			group_line_edit.set_text(group_names[index])
+		else:
+			if index >= new_group_count:
+				# Remove old Lineedits
+				group_rows.get_child(new_group_count).queue_free()
+			else:
+				# Update existing lineedit
+				var group_line_edit = group_rows.get_child(index)
+				group_line_edit.set_text(group_names[index])
+
