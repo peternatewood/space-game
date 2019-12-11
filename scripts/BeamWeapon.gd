65759619f5e58c60104b1fda54fb1dd17dd75980
peternatewood <peternatewood@tutanota.com>
2019-12-11 10:28:34 -0500

    Add timer to beam weapons to properly sync audio

--- scripts/BeamWeapon.gd
+++ scripts/BeamWeapon.gd
@@ -10,13 +10,16 @@ export (float) var warm_up_duration = 2.0
 export (String) var weapon_name = "beam weapon"
 
 onready var beam = get_node("Beam")
+onready var beam_sound_player = get_node("Beam Sound Player")
+onready var cool_down_player = get_node("Cool Down Player")
 onready var core = get_node("Core")
 onready var raycast_container = get_node("Raycast Container")
 onready var target_raycast = get_node("Raycast Container/RayCast")
+onready var timer = get_node("Timer")
+onready var warm_up_player = get_node("Warm Up Player")
 
 var beam_vector: Vector3 = Vector3.ONE
 var camera: Camera
-var countdown: float
 var current_target
 var firing_state: int = NONE
 var has_target: bool = false
@@ -26,61 +29,69 @@ var target_pos: Vector3
 func _ready():
 	camera = get_viewport().get_camera()
 
+	timer.connect("timeout", self, "_on_timer_timeout")
+	timer.set_wait_time(LOOK_FOR_TARGETS_COUNTDOWN)
+	timer.start()
+
 	beam.hide()
 	core.hide()
 
 
+func _on_timer_timeout():
+	match firing_state:
+		NONE:
+			if has_target and is_clear_line_to_target():
+				set_firing_state(WARMING_UP)
+			else:
+				# Look for other targets?
+				timer.set_wait_time(LOOK_FOR_TARGETS_COUNTDOWN)
+				timer.start()
+		WARMING_UP:
+			if has_target:
+				set_firing_state(FIRING)
+			else:
+				set_firing_state(COOLING_DOWN)
+		FIRING:
+			set_firing_state(COOLING_DOWN)
+		COOLING_DOWN:
+			set_firing_state(NONE)
+
+
 func _process(delta):
-	if has_target:
-		if countdown > 0:
-			countdown -= delta
+	var countdown = timer.get_time_left()
 
-		match firing_state:
-			NONE:
-				if countdown <= 0:
-					if is_clear_line_to_target():
-						set_firing_state(WARMING_UP)
+	match firing_state:
+		NONE:
+			pass
+		WARMING_UP:
+			core.set_scale(Vector3.ONE * (1 - (countdown / warm_up_duration)))
+		FIRING:
+			raycast_container.look_at(target_pos, Vector3.UP)
+
+			if target_raycast.is_colliding():
+				var collider = target_raycast.get_collider()
+
+				if collider is ActorBase:
+					collider._deal_damage(delta * hull_damage)
+				elif collider is ShieldQuadrant:
+					var collider_parent = collider.get_parent()
+
+					if collider.hitpoints <= 0:
+						if collider_parent is ActorBase:
+							collider_parent._deal_damage(delta * hull_damage)
 					else:
-						# Look for other targets?
-						pass
-			WARMING_UP:
-				core.set_scale(Vector3.ONE * (1 - (countdown / warm_up_duration)))
+						collider._damage(delta * shield_damage)
 
-				if countdown <= 0:
-					set_firing_state(FIRING)
-			FIRING:
-				raycast_container.look_at(target_pos, Vector3.UP)
+			# TODO: Resize the beam in case another ship crosses it
 
-				if target_raycast.is_colliding():
-					var collider = target_raycast.get_collider()
+			beam.look_at(target_pos, camera.transform.basis.x)
+			# look_at resets the scale so we have to force it again
+			beam.set_scale(beam_vector)
 
-					if collider is ActorBase:
-						collider._deal_damage(delta * hull_damage)
-					elif collider is ShieldQuadrant:
-						var collider_parent = collider.get_parent()
-
-						if collider.hitpoints <= 0:
-							if collider_parent is ActorBase:
-								collider_parent._deal_damage(delta * hull_damage)
-						else:
-							collider._damage(delta * shield_damage)
-
-				# TODO: Resize the beam in case another ship crosses it
-
-				beam.look_at(target_pos, camera.transform.basis.x)
-				# look_at resets the scale so we have to force it again
-				beam.set_scale(beam_vector)
-
-				if not beam.visible:
-					beam.show()
-
-				if countdown <= 0:
-					set_firing_state(COOLING_DOWN)
-			COOLING_DOWN:
-				core.set_scale(Vector3.ONE * (countdown / warm_up_duration))
-
-				if countdown <= 0:
-					set_firing_state(NONE)
+			if not beam.visible:
+				beam.show()
+		COOLING_DOWN:
+			core.set_scale(Vector3.ONE * (countdown / warm_up_duration))
 
 
 # PUBLIC
@@ -106,20 +117,26 @@ func set_firing_state(new_state: int):
 
 	match firing_state:
 		NONE:
-			countdown += fire_delay
+			timer.set_wait_time(fire_delay)
 			core.hide()
 		WARMING_UP:
-			countdown += warm_up_duration
+			timer.set_wait_time(warm_up_duration)
 			core.set_scale(Vector3.ZERO)
 			core.show()
+			warm_up_player.play()
 		FIRING:
-			countdown += fire_duration
+			timer.set_wait_time(fire_duration)
 			# Fire at target's position at this moment, allowing fast ships to evade
 			target_pos = current_target.transform.origin
 			beam_vector.z = global_transform.origin.distance_to(current_target.transform.origin)
+			beam_sound_player.play()
 		COOLING_DOWN:
-			countdown += warm_up_duration
+			timer.set_wait_time(warm_up_duration)
 			beam.hide()
+			beam_sound_player.stop()
+			cool_down_player.play()
+
+	timer.start()
 
 
 func set_target(target_node):
@@ -129,3 +146,5 @@ func set_target(target_node):
 
 const ActorBase = preload("ActorBase.gd")
 const ShieldQuadrant = preload("ShieldQuadrant.gd")
+
+const LOOK_FOR_TARGETS_COUNTDOWN: float = 2.0
