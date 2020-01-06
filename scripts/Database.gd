a14951f382084b5fe7551f67e434f94cfba00c2d
peternatewood <peternatewood@tutanota.com>
2020-01-06 12:07:46 -0500

    Add back button to Database menu

--- scripts/Database.gd
+++ scripts/Database.gd
@@ -1,14 +1,15 @@
 extends Control
 
+onready var loader = get_node("/root/SceneLoader")
 onready var mission_data = get_node("/root/MissionData")
 onready var preview_viewport = get_node("Ship Preview Viewport")
-onready var ship_label = get_node("Tabs/Ships/Ship Preview Rows/Ship Preview Label")
-onready var weapon_label = get_node("Tabs/Weapons/Weapon Preview Rows/Weapon Preview Label")
-onready var weapon_preview = get_node("Tabs/Weapons/Weapon Preview Rows/Weapon Preview")
+onready var ship_label = get_node("Database Rows/Tabs/Ships/Ship Preview Rows/Ship Preview Label")
+onready var weapon_label = get_node("Database Rows/Tabs/Weapons/Weapon Preview Rows/Weapon Preview Label")
+onready var weapon_preview = get_node("Database Rows/Tabs/Weapons/Weapon Preview Rows/Weapon Preview")
 
 
 func _ready():
-	var ship_list = get_node("Tabs/Ships/Ship List Scroll Container/Ship List")
+	var ship_list = get_node("Database Rows/Tabs/Ships/Ship List Scroll Container/Ship List")
 
 	for ship_name in mission_data.ship_data.keys():
 		var ship_button = Button.new()
@@ -20,7 +21,7 @@ func _ready():
 
 	_on_ship_button_pressed("Spider Fighter")
 
-	var weapon_list = get_node("Tabs/Weapons/Weapon List Scroll Container/Weapon List")
+	var weapon_list = get_node("Database Rows/Tabs/Weapons/Weapon List Scroll Container/Weapon List")
 
 	for energy_weapon_name in mission_data.energy_weapon_data.keys():
 		var energy_weapon_button = Button.new()
@@ -40,6 +41,13 @@ func _ready():
 
 	weapon_preview.connect("finished", weapon_preview, "play")
 	_on_energy_weapon_button_pressed("Energy Bolt")
+
+	var back_button = get_node("Database Rows/Back Button")
+	back_button.connect("pressed", self, "_on_back_pressed")
+
+
+func _on_back_pressed():
+	loader.load_scene("res://title.tscn")
 
 
 func _on_energy_weapon_button_pressed(energy_weapon_name: String):
