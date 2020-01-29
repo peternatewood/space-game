3679881d0c72645bcf90478822ca85e88142abd7
peternatewood <peternatewood@tutanota.com>
2020-01-29 10:38:20 -0500

    Remove arrow key camera movement from editor

--- editor/scripts/Camera.gd
+++ editor/scripts/Camera.gd
@@ -2,17 +2,6 @@ extends Spatial
 
 onready var camera = get_node("Camera")
 
-var move_vel: Vector2
-
-
-func _process(delta):
-	var move_acc: Vector2 = Vector2(Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right"), Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down"))
-
-	move_vel = move_vel.linear_interpolate(4 * move_acc, 7 * delta)
-
-	if move_vel.x != 0 or move_vel.y != 0:
-		move_position(move_vel)
-
 
 # PUBLIC
 
