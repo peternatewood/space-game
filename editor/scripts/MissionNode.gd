afaaa22087f3a181f636bf2d3d3a6573f23bd4fb
peternatewood <peternatewood@tutanota.com>
2020-01-30 15:24:08 -0500

    Add change mission button with popup to mission node

--- editor/scripts/MissionNode.gd
+++ editor/scripts/MissionNode.gd
@@ -1,9 +1,25 @@
 extends Control
 
+onready var change_mission_dialog = get_node("Change Mission Dialog")
 onready var description = get_node("Rows/Description")
 onready var title_label = get_node("Rows/Title Label")
+onready var mission_options = get_node("Change Mission Dialog/Mission Options")
 
 var mission_path: String
+
+
+func _ready():
+	change_mission_dialog.connect("confirmed", self, "_on_change_mission_confirmed")
+
+	var change_mission_button = get_node("Rows/Change Mission Button")
+	change_mission_button.connect("pressed", change_mission_dialog, "popup_centered")
+
+
+func _on_change_mission_confirmed():
+	pass
+
+
+# PUBLIC
 
 
 func set_mission(path: String, mission_name: String, mission_description: String):
