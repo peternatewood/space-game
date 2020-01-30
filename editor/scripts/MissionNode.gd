832674114cc163e4f0697e4af504e88e1dfd02d4
peternatewood <peternatewood@tutanota.com>
2020-01-30 17:21:36 -0500

    Add delete dialog to delete missions in campaign editor

--- editor/scripts/MissionNode.gd
+++ editor/scripts/MissionNode.gd
@@ -3,6 +3,7 @@ extends Control
 onready var add_mission_dialog = get_node("Add Mission Dialog")
 onready var add_mission_options = get_node("Add Mission Dialog/Rows/Mission Options")
 onready var change_mission_dialog = get_node("Change Mission Dialog")
+onready var delete_dialog = get_node("Delete Dialog")
 onready var description = get_node("Mission Container/Rows/Description")
 onready var title_label = get_node("Mission Container/Rows/Title Label")
 onready var mission_options = get_node("Change Mission Dialog/Rows/Mission Options")
@@ -21,6 +22,11 @@ func _ready():
 	add_mission_button.connect("pressed", add_mission_dialog, "popup_centered")
 
 	add_mission_dialog.connect("confirmed", self, "_on_add_mission_confirmed")
+
+	var delete_button = get_node("Mission Container/Rows/Buttons Container/Delete Button")
+	delete_button.connect("pressed", delete_dialog, "popup_centered")
+
+	delete_dialog.connect("confirmed", self, "queue_free")
 
 
 func _on_change_mission_confirmed():
@@ -51,6 +57,7 @@ func set_mission(mission_data: Dictionary):
 	mission_path = mission_data.path
 	title_label.set_text(mission_data.name)
 	description.set_text(mission_data.description)
+	delete_dialog.set_title("Delete " + mission_data.name + " mission")
 
 
 func set_mission_options(missions: Array):
