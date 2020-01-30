021787244ebe90abcbca0b4b2c69cdf94c018387
peternatewood <peternatewood@tutanota.com>
2020-01-30 15:17:14 -0500

    Add first mission node when starting campaign editor

--- editor/scripts/CampaignEditor.gd
+++ editor/scripts/CampaignEditor.gd
@@ -1,1 +1,27 @@
 extends Control
+
+onready var missions_container = get_node("Rows/Missions Panel/Missions Scroll/Missions Container")
+
+var first_mission
+
+
+func _ready():
+	create_new_campaign()
+
+
+# PUBLIC
+
+
+func create_new_campaign():
+	for row in missions_container.get_children():
+		row.queue_free()
+
+	var hbox = HBoxContainer.new()
+	missions_container.add_child(hbox)
+	hbox.set_alignment(BoxContainer.ALIGN_CENTER)
+
+	first_mission = MISSION_NODE.instance()
+	hbox.add_child(first_mission)
+
+
+const MISSION_NODE = preload("res://editor/prefabs/mission_node.tscn")
