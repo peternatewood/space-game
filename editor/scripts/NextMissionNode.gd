11f5aad2f2ae358ff365d92c37bd9654471f677d
peternatewood <peternatewood@tutanota.com>
2020-01-31 11:59:50 -0500

    Add delete button to next mission node

--- editor/scripts/NextMissionNode.gd
+++ editor/scripts/NextMissionNode.gd
@@ -1,12 +1,15 @@
 extends Control
 
-onready var mission_option = get_node("Rows/Mission Option")
+onready var mission_option = get_node("Rows/Columns/Mission Option")
 onready var objective_rows =  get_node("Rows/Objective Rows")
 
 
 func _ready():
 	var add_objective_requirement_button = get_node("Rows/Add Objective Requirement Button")
 	add_objective_requirement_button.connect("pressed", self, "_on_add_objective_requirement_pressed")
+
+	var delete_button = get_node("Rows/Columns/Delete Button")
+	delete_button.connect("pressed", self, "queue_free")
 
 
 func _on_add_objective_requirement_pressed():
